9b040f7e5f3eadaaa08b294615d3f9f13f67288e
Enaidmas <111311138+Enaidmas@users.noreply.github.com>
2022-10-03 15:00:48 -0500

    Added bootstrap & buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderDashboardComponent
   ],
   imports: [
     BrowserModule
